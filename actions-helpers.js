1dfeee34e3dedaa3482c250454dbcddbdf5d160b
Maciej Łebkowski <m.lebkowski@gmail.com>
2024-05-16 10:16:40 +0200

    use `findInput` as the flag is not mandatory

--- actions-helpers.js
+++ actions-helpers.js
@@ -1,14 +1,19 @@
 const { readFileSync } = require("fs");
 
-function getInput(name) {
+function findInput(name) {
   const val = process.env[`INPUT_${name.replace(/ /g, "_").toUpperCase()}`];
+  return val ? val.trim() : "";
+}
+
+function getInput(name) {
+  const val = findInput(name);
   if (!val) {
     throw new Error(`Input required and not supplied: ${name}`);
   }
-  return val.trim();
+  return val;
 }
 
 const getEventPayload = () =>
   JSON.parse(readFileSync(process.env.GITHUB_EVENT_PATH, { encoding: "utf8" }));
 
-module.exports = { getInput, getEventPayload };
+module.exports = { getInput, findInput, getEventPayload };
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { TrelloSdk } = require("./trello-sdk");
 const { upsertWithDependencies } = require("./upsert-pull-request-on-card");
-const { getInput, getEventPayload } = require("./actions-helpers");
+const { getInput, findInput, getEventPayload } = require("./actions-helpers");
 const { attachTrelloCards } = require("./attach-trello-cards");
 
 const isReq = ["1", "on", "yes", "true"].includes(
-  getInput("required").toLowerCase(),
+  findInput("required").toLowerCase(),
 );
 const trello = new TrelloSdk(
   getInput("trello-api-key"),
